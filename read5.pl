888117b45c3c9fdfdd15aa0b39eb957cc2828e65
Rishi Nag <rishi@ebi.ac.uk>
2015-07-30 14:03:56 +0100

    initial read test completed

--- read5.pl
+++ read5.pl
@@ -5,19 +5,32 @@
 #
 use Bio::DB::Sam ;
 
+use strict ;
 
-@test_files = ('data/some nonsense','data/yeast.sorted.cram','data/yeast.sorted.bam','data/yeast.unsorted.sam') ;
+my @test_files = ('data/file fail to open test','data/yeast.sorted.cram','data/yeast.sorted.bam','data/yeast.unsorted.sam') ;
 
 
-for $f (@test_files)
+for my $f (@test_files)
 {
   printf "\nGetting Reads from $f\n" ;
-  $read_fh = Bio::DB::HTS->open($f,"r") ;
+  my $read_fh = Bio::DB::HTS->open($f,"r") ;
   if( $read_fh )
   {
     printf( "\tFile open successful.\n" ) ;
-    
+
+    my $header = $read_fh->header_read() ;
+
+    for(my $i=0 ; $i <= 5 ; $i++)
+    {
+      my $alignment = Bio::DB::HTS::Alignment->new() ;
+      $read_fh->read1($header,$alignment) ;
+      my $qseq = $alignment->qseq() ;
+      printf( "raw alignment details:" ) ;
+      printf( "$qseq\n" ) ;
+    }
+
     $read_fh->close() ;
+    printf( "\tFile closed.\n" ) ;
   }
   else
   {
