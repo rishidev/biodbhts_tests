86f37103f773fd2197573a8d475744116dea3314
Rishi Nag <rishi@ebi.ac.uk>
2015-08-07 16:43:39 +0100

    initial display alignments setup

---
+++ display_alignments.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+
+#
+# Display alignments across SAM/BAM/CRAM file formats.
+#
+use Bio::DB::HTS ;
+
+use strict ;
+
+my @test_files = ('data/file fail to open test','data/yeast.sorted.cram','data/yeast.sorted.bam','data/yeast.unsorted.sam') ;
+
+
+for my $f (@test_files)
+{
+  printf "\nGetting Reads from $f\n" ;
+  my $read_fh = Bio::DB::HTS->open($f,"r") ;
+  if( $read_fh )
+  {
+    printf( "\tFile open successful.\n" ) ;
+
+    my $header = $read_fh->header_read() ;
+
+    for(my $i=0 ; $i <= 5 ; $i++)
+    {
+      my $alignment = Bio::DB::HTS::Alignment->new() ;
+      $read_fh->read1($header,$alignment) ;
+      my $qseq = $alignment->qseq() ;
+      printf( "raw alignment details:" ) ;
+      printf( "$qseq\n" ) ;
+    }
+
+    $read_fh->close() ;
+    printf( "\tFile closed.\n" ) ;
+  }
+  else
+  {
+    printf( "\tError:File not opened.\n" ) ;
+  }
+}
+
+
+
+
+
+
