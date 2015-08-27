cfd12300ce636b77010c53cdb0a90bd90446d708
Rishi Nag <rishi@ebi.ac.uk>
2015-08-27 14:11:03 +0100

    moved new code

---
+++ run_tests_hts.sh
@@ -0,0 +1,5 @@
+
+export PERL5LIB=/home/rishi/coding/hts_dev/Bio-HTS/lib
+export LD_LIBRARY_PATH=/home/rishi/coding/hts_dev/htslib
+perl -I /home/rishi/coding/hts_dev/Bio-HTS/blib/arch/auto/Bio/DB/HTS read5.pl
+perl -I /home/rishi/coding/hts_dev/Bio-HTS/blib/arch/auto/Bio/DB/HTS display_alignments.pl
