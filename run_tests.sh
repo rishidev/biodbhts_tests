53a2bffd2f80e7af3230a7ad892541a2e39f7f4d
Rishi Nag <rishi@ebi.ac.uk>
2015-08-27 08:27:24 +0100

    script to run tests adde

---
+++ run_tests.sh
@@ -0,0 +1,5 @@
+
+export PERL5LIB=/home/rishi/coding/htslib_for_biodbsam/Bio-HTS/lib
+export LD_LIBRARY_PATH=/home/rishi/coding/htslib_for_biodbsam/htslib
+perl -I /home/rishi/coding/htslib_for_biodbsam/Bio-HTS/blib/arch/auto/Bio/DB/HTS read5.pl
+perl -I /home/rishi/coding/htslib_for_biodbsam/Bio-HTS/blib/arch/auto/Bio/DB/HTS display_alignments.pl
