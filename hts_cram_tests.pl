0a4ebb177ae5ef2ee841d10327850cadb621f24f
Rishi Nag <rishi@ebi.ac.uk>
2016-01-13 14:47:09 +0000

    first Bio-HTS test working

--- hts_cram_tests.pl
+++ hts_cram_tests.pl
@@ -5,8 +5,11 @@
 use Bio::DB::HTS;
 use Bio::DB::HTS::AlignWrapper;
 
-my $cramfile = "$Bin/data/yeast.cram";
+my $cramfile = "data/yeast.sorted.cram";
 my $hts_file     = Bio::DB::HTSfile->open($cramfile);
 my $header  = $hts_file->header_read();
 my $targets = $header->n_targets;
 printf( "n_targets value is $targets\n" ) ;
+
+my $target_names = $header->target_name;
+my $target_lens = $header->target_len;
