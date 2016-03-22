d301e87bdcabb8d34b09fe3f49ca4502a92da9d5
Rishi Nag <rishi@ebi.ac.uk>
2016-03-22 16:53:53 +0000

    Build.PL tests added

--- build_options.sh
+++ build_options.sh
@@ -6,11 +6,170 @@
 # $2 - the test to be run
 #
 
-
+#
 #test the Build.PL with various options
+#
+if [ "$2" = "BUILD" ]; then
+    echo Build.PL on its own
+    $1
+    cd Bio-HTS
+    perl Build.PL
+    ./Build
+    cd t
+    for f in $(ls *.t) ;
+    do
+        perl $f
+    done
+    echo "Completed $2"
+    echo "Needs htslib installed or on LD_LIBRARY_PATH to pass"
+    exit 0
+fi
 
 
+if [ "$2" = "BUILD_SYSTEM_INSTALLED_HTSLIB" ]; then
+    echo Installs htslib, then runs Build process
+    git clone -b master --depth=1 https://github.com/samtools/htslib.git
+    cd htslib
+    sudo make install
+    cd ..
+    $1
+    cd Bio-HTS
+    perl Build.PL
+    ./Build
+    cd t
+    for f in $(ls *.t) ;
+    do
+        perl $f
+    done
+    echo "Completed $2"
+    exit 0
+fi
+
+if [ "$2" = "BUILD_LOCAL_INSTALLED_HTSLIB" ]; then
+    echo Installs htslib to a local dir, then runs Build process
+    git clone -b master --depth=1 https://github.com/samtools/htslib.git
+    cd htslib
+    make install --prefix=~/localsw
+    export HTSLIB_DIR=
+    cd ..
+    $1
+    cd Bio-HTS
+    perl Build.PL --prefix=~/localsw
+    ./Build
+    cd t
+    for f in $(ls *.t) ;
+    do
+        perl $f
+    done
+    echo "Completed $2"
+    exit 0
+fi
+
+
+
+if [ "$2" = "BUILD_HTSLIB_DIR_ENV" ]; then
+    echo Installs htslib, then runs Build process
+    git clone -b master --depth=1 https://github.com/samtools/htslib.git
+    cd htslib
+    make install
+    export HTSLIB_DIR="$PWD"
+    echo $HTSLIB_DIR
+    cd ..
+    $1
+    cd Bio-HTS
+    perl Build.PL
+    ./Build
+    cd t
+    for f in $(ls *.t) ;
+    do
+        perl $f
+    done
+    echo "Completed $2"
+    exit 0
+fi
+
+if [ "$2" = "BUILD_HTSLIB_DIR_FLAG" ]; then
+    echo Installs htslib, then runs Build process
+    git clone -b master --depth=1 https://github.com/samtools/htslib.git
+    cd htslib
+    make install
+    export HTSLIB_DIR_FOR_FLAG="$PWD"
+    echo $HTSLIB_DIR_FOR_FLAG
+    cd ..
+    $1
+    cd Bio-HTS
+    perl Build.PL --htslib=$HTSLIB_DIR_FOR_FLAG
+    ./Build
+    cd t
+    for f in $(ls *.t) ;
+    do
+        perl $f
+    done
+    echo "Completed $2"
+    exit 0
+fi
+
+if [ "$2" = "BUILD_HTSLIB_DIR_WITH_STATIC_FLAG" ]; then
+    echo Installs htslib, then runs Build process
+    git clone -b master --depth=1 https://github.com/samtools/htslib.git
+    cd htslib
+    make install
+    export HTSLIB_DIR_FOR_FLAG="$PWD"
+    echo $HTSLIB_DIR_FOR_FLAG
+    cd ..
+    $1
+    cd Bio-HTS
+    perl Build.PL --htslib=$HTSLIB_DIR_FOR_FLAG --static
+    ./Build
+    rm -rf $HTSLIB_DIR_FOR_FLAG
+    cd t
+    for f in $(ls *.t) ;
+    do
+        perl $f
+    done
+    echo "Completed $2"
+    exit 0
+fi
+
+
+#TODO Alien::HTSlib dependency resolver
+#TODO pkg-config test
+
+
+#
 #test the INSTALL.pl script with various options
+#
+
+if [ "$2" = "INSTALL" ]; then
+    echo INSTALL.pl on its own
+    $1
+    cd Bio-HTS
+    export STATIC_HTS=
+    perl INSTALL.pl
+    cd t
+    for f in $(ls *.t) ;
+    do
+        perl $f
+    done
+    echo "Completed $2"
+    exit 0
+fi
+
+
+if [ "$2" = "INSTALL_PREFIX_ENV" ]; then
+    echo INSTALL.pl with a specified HTSLIB_DIR environment variable
+    $1
+    cd Bio-HTS
+    export STATIC_HTS=
+    perl INSTALL.pl
+    cd t
+    for f in $(ls *.t) ;
+    do
+        perl $f
+    done
+    echo "Completed $2"
+    exit 0
+fi
 
 
 if [ "$2" = "INSTALL_STATIC_ENV" ]; then
@@ -25,7 +184,7 @@ if [ "$2" = "INSTALL_STATIC_ENV" ]; then
         perl $f
     done
     export STATIC_HTS=
-    echo "Completed"
+    echo "Completed $2"
     exit 0
 fi
 
@@ -41,7 +200,9 @@ if [ "$2" = "INSTALL_STATIC_FLAG" ]; then
     do
         perl $f
     done
-    echo "Completed"
+    echo "Completed $2"
     exit 0
 fi
 
+
+echo Build test option $2 not found
