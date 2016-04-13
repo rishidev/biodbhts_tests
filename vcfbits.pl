dc838641607519c6d87154e4fa3e60ed398b5afa
Rishi Nag <rishi@ebi.ac.uk>
2016-04-13 13:49:25 +0100

    VCFSweep is a package, not a module

--- vcfbits.pl
+++ vcfbits.pl
@@ -2,10 +2,9 @@ use strict ;
 use warnings;
 
 use Bio::DB::HTS::VCF;
-use Bio::DB::HTS::VCFSweep;
 
 #Tests for the sweep functionality
-my $sweep = Bio::DB::HTS::VCFSweep->new("data.test.vcf");
+my $sweep = Bio::DB::HTS::VCFSweep->new("data/test.vcf");
 print("File opened\n");
 $sweep->close() ;
 print("File closed\n");
