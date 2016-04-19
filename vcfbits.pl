4021af6d0643624cc9dd74dd2273ece69f0081cd
Rishi Nag <rishi@ebi.ac.uk>
2016-04-19 12:44:46 +0100

    sweep usage

--- vcfbits.pl
+++ vcfbits.pl
@@ -4,8 +4,19 @@ use warnings;
 use Bio::DB::HTS::VCF;
 
 #Tests for the sweep functionality
-my $sweep = Bio::DB::HTS::VCFSweep->new(filename => "data/test.vcf");
+#Open the file
+my $sweep = Bio::DB::HTS::VCFSweep->new(filename => "data/test.vcf.gz");
 print("File opened\n");
+
+#Display contents
+print("Header\n");
+print($sweep->header."\n");
+
+my $row1=$sweep->next_row();
+my $row2=$sweep->next_row();
+my $row1again=$sweep->previous_row();
+
+#Close
 $sweep->close() ;
 print("File closed\n");
 
