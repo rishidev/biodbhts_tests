ea73babac07ccf1566aaf50c34eb800aa8bfb0b9
Rishi Nag <rishi@ebi.ac.uk>
2016-09-02 15:08:02 +0100

    reflect repo name change

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
-export PERL5LIB=/home/rishi/coding/hts_dev/Bio-HTS/lib
+export PERL5LIB=/home/rishi/coding/hts_dev/Bio-DB-HTS/lib
 export LD_LIBRARY_PATH=/home/rishi/coding/hts_dev/htslib
 
-perl -I /home/rishi/coding/hts_dev/Bio-HTS/blib/arch/auto/Bio/DB/HTS read5.pl
-perl -I /home/rishi/coding/hts_dev/Bio-HTS/blib/arch/auto/Bio/DB/HTS display_alignments.pl
-perl -I /home/rishi/coding/hts_dev/Bio-HTS/blib/arch/auto/Bio/DB/HTS get_features.pl
+perl -I /home/rishi/coding/hts_dev/Bio-DB-HTS/blib/arch/auto/Bio/DB/HTS read5.pl
+perl -I /home/rishi/coding/hts_dev/Bio-DB-HTS/blib/arch/auto/Bio/DB/HTS display_alignments.pl
+perl -I /home/rishi/coding/hts_dev/Bio-DB-HTS/blib/arch/auto/Bio/DB/HTS get_features.pl
