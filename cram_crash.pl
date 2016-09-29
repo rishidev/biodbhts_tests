3c072ba1380ffd43dc541f202e4d03a466c72667
Rishi Nag <rishi@ebi.ac.uk>
2016-09-29 09:31:14 +0100

    cram sim enhancements

--- cram_crash.pl
+++ cram_crash.pl
@@ -9,12 +9,59 @@ use Data::Dumper;
 my $DEBUG = 1;
 my $url = "ftp://ftp.ensembl.org/pub/release-77/bam/homo_sapiens/genebuild/GRCh38.illumina.adipose.1.bam" ;
 
+my $chr = "17" ;
+my $start = "64204817" ;
+my $end = "64205728" ;
+my $starti = 64204817 ;
+my $endi = 64205728 ;
 
-print "Using Bio::DB::HTS\n" ;
+print "Using Bio::DB::HTS Directly\n" ;
+my $hts_obj = Bio::DB::HTS->new(-bam => $url) ;
+my @hts_alignments = $hts_obj->get_features_by_location(-seq_id  => $chr,
+                                                 -start  => $start,
+                                                 -end    => $end);
+foreach my $a (@hts_alignments)
+{
+    # where does the alignment start in the reference sequence
+    my $seqid  = $a->seq_id;
+    my $start  = $a->start;
+    my $end    = $a->end;
+    my $strand = $a->strand;
+    my $cigar  = $a->cigar_str;
+    my $paired = $a->get_tag_values('PAIRED');
+
+    # where does the alignment start in the query sequence
+    my $query_start = $a->query->start;
+    my $query_end   = $a->query->end;
+
+    my $ref_dna   = $a->dna;        # reference sequence bases
+    my $query_dna = $a->query->dna; # query sequence bases
+
+    my @scores    = $a->qscore;     # per-base quality scores
+    my $match_qual= $a->qual;       # quality of the match
+ }
+#$hts_obj->close($hts_obj->) ;
+
 
 
 print "Using ensembl-io\n" ;
 my $io_adaptor = Bio::EnsEMBL::IO::Adaptor::HTSAdaptor->new($url);
-$io_adaptor->fetch_alignments_filtered("17","64205516","64205825");
+$io_adaptor->fetch_alignments_filtered($chr, $start, $end);
+$io_adaptor->htsfile_close() ;
 
-print "Exiting\n" ;
+
+
+print "Using ensembl-web style - get_data\n" ;
+my $web_adaptor = Bio::EnsEMBL::IO::Adaptor::HTSAdaptor->new($url);
+my $data = $web_adaptor->fetch_alignments_filtered($chr, $start, $end) ;
+$web_adaptor->htsfile_close;
+
+print "Using ensembl-web style - consensus_features\n" ;
+
+
+print "Using ensembl-web style - consensus_features\n" ;
+
+
+
+
+print "Done - Exiting\n" ;
